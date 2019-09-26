8a3158a861a7f961d0a09f55cb74781db79a7d48
Bryan Sullivan <bryan.sullivan@att.com>
2019-09-25 17:54:29 -0700

    Jenkins integration
    Issue-ID: ACUMOS-3181
    
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>
    Change-Id: I9c9c000a099096768b7b6bda5aba665e8fca063f
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- src/main/java/org/acumos/deploymentclient/service/DeploymentService.java
+++ src/main/java/org/acumos/deploymentclient/service/DeploymentService.java
@@ -1,3 +1,22 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
 package org.acumos.deploymentclient.service;
 
 import org.acumos.cds.domain.MLPTask;
@@ -10,7 +29,6 @@ import org.springframework.stereotype.Service;
 public interface DeploymentService {
 	
 	public boolean deployValidation(DeployBean deployBean);
-	//void getSolutionZip(SolutionBean solutionBean);
 	public String getSolutionCode(String solutionId,String datasource,String userName,String dataPd);
 	public String getSingleImageData(String solutionId,String revisionId,String datasource,String userName,
 			String dataPd)throws Exception;
